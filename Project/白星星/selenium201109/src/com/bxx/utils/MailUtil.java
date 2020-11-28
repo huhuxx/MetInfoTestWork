28cc1141b42ce9d7ca6a75bd909ffbabb28aa729
baixngxn <1336124020@qq.com>
2020-11-28 14:04:50 +0800

    11.28

--- Project/白星星/selenium201109/src/com/bxx/utils/MailUtil.java
+++ Project/白星星/selenium201109/src/com/bxx/utils/MailUtil.java
@@ -1,7 +1,12 @@
 package com.bxx.utils;
+import java.io.FileReader;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
 import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -10,15 +15,20 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimeUtility;
+import javax.mail.internet.NewsAddress;
+import javax.swing.text.html.HTML;
 
 
 public class MailUtil {
 
 	public void sendMessage(String outcome) throws IOException {
-		String sender = "1336124020@qq.com";
+		String sender = "bai13113893039@126.com";
 		//授权码
-		String auth_code ="VZWDRLIBCRCPABRM";
+		String auth_code ="XPZNXMFQYCKBLFYW";
 		String[] to = ReadProperties.getPropertyValue("tomail").split(",");
 		Properties props = new Properties();
 		props.put("mail.transport.protocol", "smtp");
@@ -45,11 +55,23 @@ public class MailUtil {
 				message.addRecipient(Message.RecipientType.TO,new InternetAddress(to[i]));
 			}
 			message.setSubject("hello,白星星");
-			message.setText(outcome);
 			
+//			一个Multipart对象包含一个或多个bodypart对象，组成邮件正文
+			MimeMultipart mimeMultipart =new MimeMultipart();
+			//读取本地测试报告文件,将文件添加到"节点"
+			MimeBodyPart file=new MimeBodyPart();
+			SimpleDateFormat ft = new SimpleDateFormat ("yyyyMMddhhmmss");
+			String dir="D:\\eclipse\\WorkSpace201109\\selenium201109\\src\\com\\bxx\\freemarker";
 			
+			DataHandler dataHandler1 = new DataHandler(new FileDataSource(dir +"/result.html"));
+	        file.setDataHandler(dataHandler1);
+	        
+	        mimeMultipart.addBodyPart(file);
+	        message.setContent(mimeMultipart);
+			message.setSubject(outcome);
 			//3、发送邮件
 			Transport.send(message);
+			System.out.println("邮件已经发送");
 		} catch (AddressException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
