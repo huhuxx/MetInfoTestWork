bd914329b88157ee4bbb37af64914af6c462aa80
baixngxn <1336124020@qq.com>
2020-12-01 19:33:54 +0800

    11.30
    自动化测试用例50条

---
+++ Project/白星星/11.30自动化测试用例+build文件/JoinuspageTest.java
@@ -0,0 +1,75 @@
+package com.bxx.test;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class JoinuspageTest extends BaseTest {
+
+	@BeforeTest
+	public void beforeall() throws InterruptedException {
+		webtest.enterFrame1("xpath=//*[@class='page-iframe flex-fill']");
+		webtest.click("xpath=//*[@id='met-nav-collapse']/ul/li[7]/a/span[@class='editable-click']");
+	}
+	
+	@Test
+	public void text1() throws InterruptedException {
+		//选择岗位输入简历并进行提交
+		webtest.runJs("window.scrollBy(0, 400)");
+		webtest.click("xpath=//a[@data-jobid='13']");
+		Thread.sleep(5000);
+		webtest.type("xpath=//*[@id='met-job-cv0']/div/div/div[2]/form/div[1]/input", "王琦");
+		webtest.click("xpath=//*[@id='para166_2']");
+		webtest.type("xpath=//*[@id='met-job-cv0']/div/div/div[2]/form/div[3]/input", "135682634");
+		webtest.type("xpath=//*[@id='met-job-cv0']/div/div/div[2]/form/div[4]/input", "13113893039@163.com");
+		webtest.type("xpath=//*[@id='met-job-cv0']/div/div/div[2]/form/div[13]/div/input", "1");
+		webtest.click("xpath=//*[@id=\"met-job-cv0\"]/div/div/div[2]/form/div[14]/button[1]");
+		String alert  = webtest.switchtoalert();
+		Assert.assertEquals(alert,"操作成功!");
+	}
+	
+	@Test
+	public void test2() throws InterruptedException {
+		//填写简历信息并选择点击【取消】
+		webtest.runJs("window.scrollBy(0, 400)");
+		webtest.click("xpath=//a[@data-jobid='13']");
+		Thread.sleep(5000);
+		webtest.type("xpath=//*[@id='met-job-cv0']/div/div/div[2]/form/div[1]/input", "Lili");
+		webtest.click("xpath=//*[@id='para166_2']");
+		webtest.type("xpath=//*[@id='met-job-cv0']/div/div/div[2]/form/div[3]/input", "135682634");
+		webtest.type("xpath=//*[@id='met-job-cv0']/div/div/div[2]/form/div[4]/input", "13113893039@163.com");
+		webtest.type("xpath=//*[@id='met-job-cv0']/div/div/div[2]/form/div[13]/div/input", "1");
+		webtest.click("xpath=//*[@id='met-job-cv0']/div/div/div[2]/form/div[14]/button[2]");
+	}
+	
+	@Test
+	public void test3() throws InterruptedException {
+		//搜索岗位信息，并且在其中一个岗位投递简历信息
+		webtest.type("xpath=//input[@name='content']", "测试后台人员");
+		webtest.click("xpath=/html/body/section[1]/div/div/div/form/div/button/i");
+		webtest.click("xpath=//div[@class='card card-shadow']/div[2]/a");
+		
+		webtest.type("xpath=//*[@id='met-job-cv0']/div/div/div[2]/form/div[1]/input","赵六");
+		webtest.click("xpath=//*[@id='para166_2']");
+		webtest.type("xpath=//*[@id='met-job-cv0']/div/div/div[2]/form/div[3]/input", "135682634");
+		webtest.type("xpath=//*[@id='met-job-cv0']/div/div/div[2]/form/div[4]/input", "13113893039@163.com");
+		webtest.type("xpath=//*[@id='met-job-cv0']/div/div/div[2]/form/div[14]/div/input", "1");
+		webtest.click("xpath=//*[@id='met-job-cv0']/div/div/div[2]/form/div[15]/button[1]");
+	}
+	
+	@Test
+	public void test4() throws InterruptedException {
+		//下滑至底部 ，切换至第二个页面，并返回第一个页面
+		String jString = "window.scrollTo(0,document.body.scrollHeight)";
+		webtest.runJs(jString);
+		webtest.click("xpath=/html/body/section[2]/div/div/div[2]/div/a[text()='2']");
+		Thread.sleep(5000);
+		webtest.runJs(jString);
+		webtest.click("xpath=/html/body/section[2]/div/div/div[2]/div/a[text()='上一页']");
+	}
+	
+
+}
---
+++ Project/白星星/11.30自动化测试用例+build文件/KehuanliEditTest.java
@@ -0,0 +1,240 @@
+package com.bxx.test;
+
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class KehuanliEditTest extends BaseTest {
+	@Test
+	public void testEditDelete1() throws InterruptedException {
+		webtest.click("xpath=//a[@class='btn btn-outline btn-inverse met-img-showbtn']");
+		Thread.sleep(5000);
+		webtest.mouseToElement("xpath=//div[@class='met-showimg']");
+		List<WebElement> list = webtest.backlist("xpath=/html/div[2]/div/button[2][@class='btn btn-xs btn-warning pageset-content']");
+		webtest.click(list.get(0));
+		Thread.sleep(5000);		
+		webtest.leaveFrame();
+		//Delete
+		webtest.typeAndClear("xpath=//input[@name='title']", "这是删除部分图片后的名字");
+		
+		List<WebElement> list2 = webtest.backlist("xpath=//*[@class='glyphicon glyphicon-trash text-grey']");
+		webtest.click(list2.get(1));
+		webtest.enterFrame("ueditor_0");
+		webtest.typeAndClear("xpath=//*[@class='view']", "这是删除图片后的介绍内容");
+
+		webtest.leaveFrame();
+		webtest.click("xpath=//*[@class='btn btn-primary']");
+		Thread.sleep(50000);
+	}
+	@Test
+	public void testEdit2() throws InterruptedException {
+		//将第二个放入回收站
+		webtest.click("xpath=//a[text()='内容']");
+		List<WebElement> list = webtest.backlist("xpath=//*[@class='py-2 d-block']");
+		webtest.click(list.get(4));
+		
+		List<WebElement> deleteList = webtest.backlist("xpath=//*[@class='btn btn-sm btn-default']");
+		webtest.click(deleteList.get(1));
+		webtest.click("xpath=//*[@class='btn btn-sm mr-1 btn-primary btn-table-del-recycle']");
+	}
+	@Test
+	public void testEdit3() throws InterruptedException {
+		//将第二个从回收站恢复
+		webtest.click("xpath=//a[text()='内容']");
+		webtest.click("xpath=//*[@class='fa-recycle']");
+		List<WebElement> deleteList = webtest.backlist("xpath=//*[@class='btn btn-sm btn-primary mr-1']");
+		webtest.click(deleteList.get(0));
+	}
+	@Test
+	public void testEdit4() throws InterruptedException {
+		//将回收站所有的内容删除
+		webtest.click("xpath=//a[text()='内容']");
+		webtest.click("xpath=//*[@class='fa-recycle']");
+		List<WebElement> deleteList = webtest.backlist("xpath=//*[@name='id']");
+		System.out.println(deleteList.size()+"-------------");
+		for (int i = deleteList.size(); i > 0; i--) {
+			webtest.click(deleteList.get(i-1));
+		}
+		webtest.click("xpath=//*[@class='btn btn-default']");
+		webtest.click("xpath=//*[@class='ok btn btn-primary']");
+	}
+	@Test
+	public void testEditAdd5() throws InterruptedException {
+		//添加新的内容带有本地图片
+		webtest.click("xpath=//a[text()='内容']");
+		List<WebElement> list = webtest.backlist("xpath=//*[@class='py-2 d-block']");
+		webtest.click(list.get(4));
+		webtest.click("xpath=//*[text()='添加内容']");
+		webtest.type("xpath=//*[@name='title']","这是新添加的带有本地图片内容");
+		//添加本地图片
+		webtest.type("xpath=//*[@name='imgurl']","D:\\ABCD\\1.jpg");
+		
+		webtest.enterFrame("ueditor_0");
+		webtest.type("tag=body", "这是介绍部分");
+		webtest.leaveFrame();
+		
+		webtest.click("xpath=//div[@class='float-right']//button[text()='保存']");
+	}
+	@Test
+	public void testEditAdd6() throws InterruptedException {
+		//添加新的内容
+		webtest.click("xpath=//a[text()='内容']");
+		List<WebElement> list = webtest.backlist("xpath=//*[@class='py-2 d-block']");
+		webtest.click(list.get(4));
+		webtest.click("xpath=//*[text()='添加内容']");
+		webtest.type("xpath=//*[@name='title']","这是新添加的带有仓库图片的展示");
+		//添加仓库图片
+		webtest.click("xpath=//*[@class='btn btn-default fileinput-file-choose element-visible']");
+		List<WebElement> packageList = webtest.backlist("xpath=//*[@class='fa-folder-open-o h1']");
+		System.out.println(packageList.size()+"文件夹的长度");
+		webtest.doubleClick(packageList.get(1));
+		
+		webtest.click("xpath=//*[@class='btn btn-warning float-right btn-img-folder-back']");
+		webtest.click("xpath=//*[@class='d-flex align-items-center justify-content-center p-1 border position-relative img']");
+		webtest.mouseToElement("xpath=//*[@class='float-right']");
+		
+		List<WebElement> btnList = webtest.backlist("xpath=//*[@class='btn btn-primary']");
+		System.out.println("button的长度"+btnList.size());
+		webtest.click(btnList.get(2));
+		
+		webtest.enterFrame("ueditor_0");
+		webtest.type("tag=body", "这是介绍部分");
+		webtest.leaveFrame();
+		
+		webtest.click(btnList.get(1));
+		
+	}
+	@Test
+	public void testEditAdd7() throws InterruptedException {
+		//添加新的内容带有网络图片
+		webtest.click("xpath=//a[text()='内容']");
+		List<WebElement> list = webtest.backlist("xpath=//*[@class='py-2 d-block']");
+		webtest.click(list.get(4));
+		webtest.click("xpath=//*[text()='添加内容']");
+		webtest.type("xpath=//*[@name='title']","这是新添加的带有网络图片的内容");
+		//添加网络图片
+		String imgSrc = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605677648442&di=ddaf61fc4a86c56af13714deeba2785e&imgtype=0&src=http%3A%2F%2Fa3.att.hudong.com%2F55%2F22%2F20300000929429130630222900050.jpg";
+		webtest.click("xpath=//button[@class='btn btn-default fileinput-file-other element-visible']");
+		webtest.type("xpath=//*[@name='img_url']", imgSrc);
+		//点击保存
+		List<WebElement> btnList = webtest.backlist("xpath=//*[@class='btn btn-primary']");
+		System.out.println("button的长度"+btnList.size());
+		webtest.click(btnList.get(2));
+		
+		webtest.enterFrame("ueditor_0");
+		webtest.type("tag=body", "这是介绍部分");
+		webtest.leaveFrame();
+		
+		webtest.click(btnList.get(1));
+	}
+	
+	@Test
+	public void testEditAdd8() throws InterruptedException {
+//		添加新的内容带有本地视频的内容
+		webtest.click("xpath=//a[text()='内容']");
+		List<WebElement> list = webtest.backlist("xpath=//*[@class='py-2 d-block']");
+		webtest.click(list.get(4));
+		webtest.click("xpath=//*[text()='添加内容']");
+		webtest.type("xpath=//*[@name='title']","这是新添加的带有本地视频的内容");
+		
+		webtest.enterFrame("ueditor_0");
+		webtest.type("tag=body", "这是介绍部分");
+		webtest.leaveFrame();
+
+		//设置视频内容
+		webtest.click("id=edui167_body");
+		webtest.enterFrame("edui163_iframe");
+		webtest.click("xpath=//*[@id='tabHeads']//span[text()='上传视频']");
+		webtest.type("xpath=//input[@class='webuploader-element-invisible']", "D:\\ABCD\\leave.mp4");
+		webtest.click("xpath=//*[@class='uploadBtn state-ready']");
+		webtest.leaveFrame();
+		webtest.click("id=edui165_body");
+		
+		webtest.click("xpath=//div[@class='float-right']//button[text()='保存']");	
+	}
+	
+	@Test
+	public void testEditAdd9() throws InterruptedException {
+//		添加新的内容带有网络视频的内容
+		webtest.click("xpath=//a[text()='内容']");
+		List<WebElement> list = webtest.backlist("xpath=//*[@class='py-2 d-block']");
+		webtest.click(list.get(4));
+		webtest.click("xpath=//*[text()='添加内容']");
+		webtest.type("xpath=//*[@name='title']","这是新添加的带有网络视频的内容");
+		
+		webtest.enterFrame("ueditor_0");
+		webtest.type("tag=body", "这是介绍部分");
+		webtest.leaveFrame();
+
+		//设置视频内容
+		webtest.click("id=edui167_body");
+		webtest.enterFrame("edui163_iframe");
+		webtest.type("id=videoUrl", "https://haokan.baidu.com/v?vid=8729297236018545010&pd=bjh&fr=bjhauthor&type=video");
+		Thread.sleep(5000);
+		webtest.type("id=videoWidth", "800");
+		webtest.type("id=videoHeight", "500");
+		webtest.leaveFrame();
+		webtest.click("id=edui165_body");
+		
+		webtest.click("xpath=//div[@class='float-right']//button[text()='保存']");	
+	}
+	
+	@Test
+	public void testEditAdd10() throws InterruptedException {
+//		添加新的内容带有超链接的内容
+		webtest.click("xpath=//a[text()='内容']");
+		List<WebElement> list = webtest.backlist("xpath=//*[@class='py-2 d-block']");
+		webtest.click(list.get(4));
+		webtest.click("xpath=//*[text()='添加内容']");
+		webtest.type("xpath=//*[@name='title']","这是新添加的带有超链接的内容");
+		
+		webtest.enterFrame("ueditor_0");
+		webtest.type("tag=body", "这是介绍部分");
+		webtest.leaveFrame();
+
+		//设置超链接内容
+		List<WebElement> list2 = webtest.backlist("xpath=//*[@class='edui-button-wrap edui-default']");
+		webtest.click(list2.get(21));
+		webtest.enterFrame1("xpath=//*[@class='%%-iframe edui-insertimage-iframe']");
+		webtest.type("id=text", "请打开网站首页");
+		webtest.type("id=href", "127.0.0.1:9900");
+		webtest.type("id=title", "这是一个超链接");
+		webtest.click("id=target");
+		webtest.leaveFrame();
+		
+		//点击确认
+		List<WebElement> list3 = webtest.backlist("xpath=//*[@class='edui-button-body edui-default']");
+		webtest.click(list3.get(54));
+		
+		webtest.click("xpath=//div[@class='float-right']//button[text()='保存']");	
+	}
+	
+	@Test
+	public void testEditAdd11() throws InterruptedException {
+		//详细内容中多图上传
+		webtest.click("xpath=//a[text()='内容']");
+		List<WebElement> list = webtest.backlist("xpath=//*[@class='py-2 d-block']");
+		webtest.click(list.get(4));
+		webtest.click("xpath=//*[text()='添加内容']");
+		webtest.type("xpath=//*[@name='title']","这是新添加的带有多图上传的内容");
+		
+		webtest.enterFrame("ueditor_0");
+		webtest.type("tag=body", "这是介绍部分");
+		webtest.leaveFrame();
+
+		//设置多图上传
+		List<WebElement> list2 = webtest.backlist("xpath=//*[@class='edui-button-wrap edui-default']");
+		webtest.click(list2.get(23));
+
+		webtest.enterFrame("edui153_iframe");
+		webtest.type("name=file","D:\\ABCD\\2.jpg");
+		webtest.type("name=file","D:\\ABCD\\3.jpg");
+		webtest.type("name=file","D:\\ABCD\\4.jpg");
+		webtest.click("xpath=//*[@class='uploadBtn state-ready']");
+		webtest.leaveFrame();
+		webtest.click("id=edui155_body");
+		
+		webtest.click("xpath=//div[@class='float-right']//button[text()='保存']");
+	}
+}
